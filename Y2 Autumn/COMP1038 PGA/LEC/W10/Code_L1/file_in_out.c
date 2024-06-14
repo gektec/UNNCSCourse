2beac1fe81fb08e9e8fc2e8d9452cc16ab4861e4
Changyu Li <55316375+gektec@users.noreply.github.com>
2024-06-14 17:58:57 +0800

    Delete wrong characters

--- Y2 Autumn/COMP1038 PGA/LEC/W10/Code_L1/file_in_out.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main()
-{ 
-    int i, j;
-    scanf("%d%d", &i, &j);
-    printf("Sum is: %d", i+j);
-}
--- Y2 Autumn/COMP1038 PGA/LEC/W11/Code_L2/multiple_files/f.h
+++
@@ -1,6 +0,0 @@
-#ifndef F_H_INCLUDED
-#define F_H_INCLUDED
- 
-void f1test();
-
-#endif
